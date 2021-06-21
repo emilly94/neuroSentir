9565c2d2866ea6672e7ee3db92e807c240ac1b3c
emilly94 <emillysporto.94@gmail.com>
2021-06-21 19:10:08 -0300

    Site neuroSentir 1.0

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <header class="masthead">
+            <div class="container">
+                <div class="masthead-subheading">Neuro</div>
+                <div class="masthead-heading text-uppercase">Sentir</div>
+                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Nossos serviços</a>
+            </div>
+        </header>
+  );
+}
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from "../assets/img/logo.png";
+
+const Navbar = () => {
+  return(
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+    <a class="navbar-brand" href="#page-top"><img src={logo} alt="..." /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars ms-1"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                <li class="nav-item"><a class="nav-link" href="#services"><strong>Serviços</strong></a></li>
+                <li class="nav-item"><a class="nav-link" href="#portfolio"><strong>Portfolio</strong></a></li>
+                <li class="nav-item"><a class="nav-link" href="#about"><strong>Sobre</strong></a></li>
+                <li class="nav-item"><a class="nav-link" href="#team"><strong>Psicologos</strong></a></li>
+                <li class="nav-item"><a class="nav-link" href="#contact"><strong>Contato</strong></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+  );
+}
+export default Navbar;
