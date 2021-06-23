fcc901ffac47e5ec14472b1cd66fcfe9c30a1f56
Emilly <emillysporto.94@gmail.com>
2021-06-23 11:43:51 -0300

    Update text page Team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,18 +7,18 @@ const Team = () =>{
     <section class="page-section bg-light" id="team">
             <div class="container">
                 <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Nosso time de psicologos</h2>
-                    <h3 class="section-subheading text-muted">Sinta-se acolhido.</h3>
+                    <h2 class="section-heading text-uppercase">Nosso time de psicólogos</h2>
+                    <h3 class="section-subheading text-muted">Sintam-se acolhido!</h3>
                 </div>
                 <div class="row">
                     <div class="col-lg-4">
                         <div class="team-member">
                             <img class="mx-auto rounded-circle" src={Img1} alt="..." />
-                            <h4>Parveen Anand</h4>
-                            <p class="text-muted">Lead Designer</p>
-                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                            <h4>Alessandra</h4>
+                            <p class="text-muted">Psicóloga</p>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-instagram"></i></a>
                             <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-dark btn-social mx-2" href="#!" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
                     <div class="col-lg-4">
@@ -33,7 +33,7 @@ const Team = () =>{
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
+                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Quando é percebida em determinada pessoa alguma alteração de ordem neurológica, é importante chamar a atenção para o fato de que não só em casos de transtornos ou distúrbios, mas também quando um indivíduo convive com alguma desordem ocasionada por AVC, traumatismo craniano, situações de demência, entre outras.</p></div>
                 </div>
             </div>
         </section>
