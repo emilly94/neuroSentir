ebd488475f401655da1ec2a21b5d335342431321
Emilly <emillysporto.94@gmail.com>
2021-06-23 17:08:54 -0300

    Update Image page Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ const Header = () => {
   return(
     <header class="masthead">
             <div class="container">
-                <div class="masthead-subheading">Neuro</div>
-                <div class="masthead-heading text-uppercase">Sentir</div>
-                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Nossos serviços</a>
+                <div class="masthead-subheading">Sintam-se</div>
+                <div class="masthead-heading text-uppercase">Acolhidos</div>
+                {/*<a class="btn btn-primary btn-xl text-uppercase" href="#services">Nossos serviços</a>*/}
             </div>
         </header>
   );
